e9771c3ae28118dd9d292def848e6ccaa2bb2dd4
Wayne <wayne.graham@gmail.com>
2010-04-12 21:11:58 -0400

    Added some beef to everything

--- app.rb
+++ app.rb
@@ -1,14 +1,20 @@
 require 'rubygems'
 require 'sinatra'
 
-configure :production do
-  
+configure do
+  set :views, "#{File.dirname(__FILE__)}/views"
+end
+
+error do
+  e = request.env['sinatra.error']
+  Kernel.puts e.backtrace.join("\n")
+  'Application error'
 end
 
 get '/' do
-  
+  haml :root
 end
 
-get '/env' do
-  ENV.inspect
-end
+# get '/env' do
+#   ENV.inspect
+# end
---
+++ environment.rb
@@ -0,0 +1,17 @@
+require 'rubygems'
+require 'haml'
+require 'ostruct'
+
+require 'sinatra' unless defined?(Sinatra)
+
+configure do
+  SiteConfig = OpenStruct.new(
+                 :title => 'Zotero ID Finder',
+                 :author => 'Wayne Graham',
+                 :url_base => 'http://localhost:4567/'
+               )
+               
+  # load models
+  $LOAD_PATH.unshift("#{File.dirname(__FILE__)}/lib")
+  Dir.glob("#{File.dirname(__FILE__)}/lib/*.rb") { |lib| require File.basename(lib, '.*') }
+end
